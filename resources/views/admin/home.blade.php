d1c747c670ebfd0ada94f027cbf86438995fe233
unknown <zvonimir.sose@fpmoz.sum.ba>
2024-02-23 01:10:38 +0100

    first commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    
+    <h1>Admin Dashboard</h1>
+</body>
+</html>
